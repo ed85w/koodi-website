052f5e2550023f380db24b703eef5ded3ddb59d0
ed85w <ed.walker@hotmail.co.uk>
2021-07-01 14:11:21 +0100

    testing cicles float on mob

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
 // // "floating circles"
-// const randomX = random(1, 20);
-// const randomY = random(1, 20);
-// const randomDelay = random(0, 2);
-// const randomTime = random(3, 5);
-// const randomTime2 = random(5, 10);
-// const randomAngle = random(-10, 10);
+const randomX = random(1, 20);
+const randomY = random(1, 20);
+const randomDelay = random(0, 2);
+const randomTime = random(3, 5);
+const randomTime2 = random(5, 10);
+const randomAngle = random(-10, 10);
 
-// const circles = gsap.utils.toArray('div.circle.floating');
-// circles.forEach(circle => {
-//   gsap.set(circle, {
-//     x: randomX(-1),
-//     y: randomX(1),
-//   });
+const circles = gsap.utils.toArray('div.circle.floating');
+circles.forEach(circle => {
+  gsap.set(circle, {
+    x: randomX(-1),
+    y: randomX(1),
+  });
 
-//   moveX(circle, 1);
-//   moveY(circle, -1);
-// });
+  moveX(circle, 1);
+  moveY(circle, -1);
+});
 
-// function rotate(target, direction) {
+function rotate(target, direction) {
   
-//   gsap.to(target, randomTime2(), {
-//     rotation: randomAngle(direction),
-//     delay: randomDelay(),
-//     ease: Sine.easeInOut,
-//     onComplete: rotate,
-//     onCompleteParams: [target, direction * -1]
-//   });
-// }
+  gsap.to(target, randomTime2(), {
+    rotation: randomAngle(direction),
+    delay: randomDelay(),
+    ease: Sine.easeInOut,
+    onComplete: rotate,
+    onCompleteParams: [target, direction * -1]
+  });
+}
 
-// function moveX(target, direction) {
+function moveX(target, direction) {
   
-//   gsap.to(target, randomTime(), {
-//     x: randomX(direction),
-//     ease: Sine.easeInOut,
-//     onComplete: moveX,
-//     onCompleteParams: [target, direction * -1]
-//   });
-// }
+  gsap.to(target, randomTime(), {
+    x: randomX(direction),
+    ease: Sine.easeInOut,
+    onComplete: moveX,
+    onCompleteParams: [target, direction * -1]
+  });
+}
 
-// function moveY(target, direction) {
+function moveY(target, direction) {
   
-//   gsap.to(target, randomTime(), {
-//     y: randomY(direction),
-//     ease: Sine.easeInOut,
-//     onComplete: moveY,
-//     onCompleteParams: [target, direction * -1]
-//   });
-// }
+  gsap.to(target, randomTime(), {
+    y: randomY(direction),
+    ease: Sine.easeInOut,
+    onComplete: moveY,
+    onCompleteParams: [target, direction * -1]
+  });
+}
 
-// function random(min, max) {
-//   const delta = max - min;
-//   return (direction = 1) => (min + delta * Math.random()) * direction;
-// }
+function random(min, max) {
+  const delta = max - min;
+  return (direction = 1) => (min + delta * Math.random()) * direction;
+}
 
 
 

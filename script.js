887694afbe00083b45b37ecdbabab971c5b56103
ed85w <ed.walker@hotmail.co.uk>
2021-07-08 14:38:49 +0100

    think all content is in, except portfolio

--- script.js
+++ script.js
@@ -32,7 +32,6 @@ function animateListItems() {
 }
 
 // gsap timeline to animate logo 
-
 var logoTl = gsap.timeline({})
 logoTl.from("#logo-k-line", {duration:1, height: 0, ease: Power3.easeOut }, 0.7)
 logoTl.from("#logo-i-line", {duration:1, scaleY:0, transformOrigin:"center bottom",ease: Power3.easeOut} , 0.7)
@@ -97,53 +96,31 @@ function random(min, max) {
 
 
 // SCROLLTRIGGER ANIMATIONS 
-
 gsap.registerPlugin(ScrollTrigger);
 
-
-// testimonial circle animation (mob )
-let testimonialTl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".testimonial-logo-container",
-    start: "top center",
+ScrollTrigger.matchMedia({
+  // mobile only - each animated individually
+  "(max-width: 991px)": function() {
+    // testimonial circle animation (mob )
+    let testimonialTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".testimonial-logo-container",
+        start: "top center",
+      }
+    })
+    testimonialTl.from("#koodi-logo-testimonial", {
+      width: "100%",
+      duration: 1,
+      ease: "back.out(1.7)"
+    })
+    .from(".testimonials .container .pink-circle", {
+      scale: 0,
+      duration: 1,
+      ease: "back.out(1.7)"
+    },0)
   }
 })
-testimonialTl.from(".testimonial-logo", {
-  width: "100%",
-  duration: 1,
-  ease: "back.out(1.7)"
-})
-.from(".testimonials .container .pink-circle", {
-  scale: 0,
-  duration: 1,
-  ease: "back.out(1.7)"
-},0)
 
-
-
-// let tl = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".blue-circle",
-//     start: "top 10%", //when top of element crosses 80% from of page
-//     end: "bottom center",   //when bottom of element crosses center of page
-//     toggleActions: "play none reverse none",
-//     // scrub: 1
-//   }
-// })
-
-// const blueCircle = document.getElementsByClassName("blue-circle")
-// tl.to(".blue-circle", {
-//   x: () => innerWidth * 0.9,
-//   y: () => innerHeight * 0.9,
-//   duration: 1.5,
-//   ease: "ease-in-out"
-// })
-// .to(".blue-circle", {
-//   x: blueCircle._gsTransform.x,
-//   y: blueCircle._gsTransform.y,
-//   duration: randomTime,
-//   repeat: -1
-// })
 
 // SWIPER 
 const swiper = new Swiper(".mySwiper", {
